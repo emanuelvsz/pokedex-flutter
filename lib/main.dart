b453488a8f35309fbf107af9600f6b8b088375af
jumelo-n <usermane13@gmail.com>
2022-09-01 14:38:55 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/pages/home_page.dart';
+import 'package:pokedex/pages/pokemon_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: PokePage(),
+    ),
+  );
+}
+
+
---
+++ lib/pages/pokemon_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PokePage extends StatefulWidget {
+  const PokePage({Key? key}) : super(key: key);
+
+  @override
+  State<PokePage> createState() => _PokePageState();
+}
+
+class _PokePageState extends State<PokePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text('Poke Page')
+    );
+  }
+}
